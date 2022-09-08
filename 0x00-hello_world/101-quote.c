2f70a3e49def73ab4edd3692e01b2c83cd2d4650
Bsakwa <sakwabrian23@gmail.com>
2022-09-08 21:41:15 +0000

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Prints the given statement, followed by a newline to stderr
+ *
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
