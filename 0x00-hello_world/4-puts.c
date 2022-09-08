a4e9303ca53b4f8f09c415a5ac01b8605a12a82a
Bsakwa <sakwabrian23@gmail.com>
2022-09-08 16:16:59 +0000

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'The program prints words as given'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
