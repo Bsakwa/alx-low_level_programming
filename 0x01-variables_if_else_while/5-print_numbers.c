ac2dc69be8e5310a08bba5d6a8849ee6a2da0a9c
Bsakwa <sakwabrian23@gmail.com>
2022-09-09 20:36:35 +0000

    smile in the mirror

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Description - 'Prints numbers from 0 to 10'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0 ; c < 10 ; c++)
+	{
+		printf("%d", c);
+	}
+	return (0);
+}
+
+
+
