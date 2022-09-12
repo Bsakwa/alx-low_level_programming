c8e62ac50c51ec39a14bcd57a61f0a7615a0d965
Bsakwa <sakwabrian23@gmail.com>
2022-09-12 15:52:16 +0000

    Going steady

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description - Prints combination of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int dig1, dig2;
+
+	for (dig1 = 0; dig1 < 9; dig1++)
+	{
+		for (dig2 = dig1 + 1; dig2 < 10; dig2++)
+		{
+			if ((dig1 == 9)) 
+			continue;
+			{
+				putchar((dig1 % 10) + '0');
+				putchar((dig2 % 10) + '0');
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
