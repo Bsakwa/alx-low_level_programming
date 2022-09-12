8a7a3d2fd52f648705a31e1b16be226214a04fec
Bsakwa <sakwabrian23@gmail.com>
2022-09-12 08:28:53 +0000

    Perspiration

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,8 +13,13 @@ int main(void)
 	for (num = 0; num <= 9;  num++)
 	{
 		putchar((num % 10) + '0');
-		putchar(',');
-		putchar(' ');
+		if (num == 9)
+			continue;
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
 	}
 	putchar('\n');
 
