12b10184b22d1f944a66926a59625ac1cac8de5a
Bsakwa <sakwabrian23@gmail.com>
2022-09-13 14:07:17 +0000

    My second commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<main.h>
+/**
+ * main - entry point
+ *
+ * Description - This program displays the alphabets
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+
 #include<stdio.h>
 
 int _putchar(void);
+void print_alphabet(void);
