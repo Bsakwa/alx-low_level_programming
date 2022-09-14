f77b3abf55a96305deb5e8e2e1154dad2cfafa50
Bsakwa <sakwabrian23@gmail.com>
2022-09-14 14:55:25 +0000

    Second Commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry Point
+ *
+ * Description - This program prints alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('/n');
+}
