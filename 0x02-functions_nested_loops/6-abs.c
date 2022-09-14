53948d6cf2953a8850520afa0041e30f33489b47
Bsakwa <sakwabrian23@gmail.com>
2022-09-14 18:33:36 +0000

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - Prints the absolute number of an integer
+ *
+ *
+ * Return: Always (0)
+ */
+int _abs(int)
+{
+	int t;
+
+	if (t < 0)
+	{
+		return (t);
+	}
+	else
+	{
+		return (-t);
+	}
+}
+
