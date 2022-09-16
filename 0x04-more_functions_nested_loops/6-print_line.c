fd5d6d6d2ea70b90d2eda27c5708bbc42aba7aa9
Bsakwa <sakwabrian23@gmail.com>
2022-09-16 11:31:40 +0000

    A straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,25 @@
 #include "main.h"
 /**
  * print_line - draws a straight line
+ * @n: Number of times _ is entered
  *
  * Return: n
  */
 void print_line(int n)
 {
+	int b;
 
-	char c;
-
-	c = '_';
-
-	if (n > 0)
-	{
-		_putchar(c);
-	}
-	else
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
+	else
+	{
+		for (b = 0; b < n; b++)
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
 
 }
