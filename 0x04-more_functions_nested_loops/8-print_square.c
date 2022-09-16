3948a7d4a4991f66096c68c31f80e0e090dca046
Bsakwa <sakwabrian23@gmail.com>
2022-09-16 16:16:12 +0000

    A square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ *
+ * Return: Void
+ */
+void print_square(int size)
+{
+	int z, w;
+
+	char h = 35;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (z = 0; z < size; z++)
+		{
+			for (w = 0; w < size; w++)
+			{
+				_putchar(h);
+			}
+			_putchar('\n');
+		}
+
+	}
+}
