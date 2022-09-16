f4ef8eafe1ae49e2b9994af7c6214df321ef7b0c
Bsakwa <sakwabrian23@gmail.com>
2022-09-16 19:44:47 +0000

    Prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry program
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	long int n = 612852475143;
+
+	printf(" The largest prime number is");
+	return (0);
+}
+
+
