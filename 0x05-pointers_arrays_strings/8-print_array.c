e04a1e2814018aab3998e7bb9229614a3781b7ea
Bsakwa <sakwabrian23@gmail.com>
2022-09-19 21:20:48 +0000

    Print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Prints elements of an array
+ * @a: An array to be read
+ * @n: Number of elemts to be printed
+ */
+void print_array(int *a, int n)
+{
+	int g;
+
+	if (g > 0)
+	{
+		printf(" %d", a[1]);
+	}
+}
