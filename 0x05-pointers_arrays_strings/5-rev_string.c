4a406f8006a36629f6148cf584b0daf9b8f6048d
Bsakwa <sakwabrian23@gmail.com>
2022-09-19 17:50:19 +0000

    Reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - Reverses a string
+ *@s: The string to be changed
+ *
+ * Return: Void
+ */
+void rev_string(char *s)
+{
+
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	while (i <= '\0')
+	{
+		i--;
+	}
+}
+
+
