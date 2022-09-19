3a2c26b98f1169f658e530ad41bb9a9edc7cad9a
Bsakwa <sakwabrian23@gmail.com>
2022-09-19 09:03:00 +0000

    Second commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,14 +2,16 @@
 /**
  * swap_int - swaps the value of two integers
  * @a: input parameter
- * !b: input parameter
+ * @b: input parameter
  *
  * Return: void
  */
 void swap_int(int *a, int *b)
 {
 	int val;
+
 	val = *a;
 	*a = *b;
 	*b = val;
+
 }
