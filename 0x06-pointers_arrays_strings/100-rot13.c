1ebae0b595564ad5e2cc4081fb70f68643c125c1
Bsakwa <sakwabrian23@gmail.com>
2022-09-22 17:45:26 +0000

    Magic

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * rot13 - replaces every letter with the 13 letter from it
+ * @p: letter to be replaced
+ *
+ * return: The modified string
+ */
+char *rot13(char *p)
+{
+	int a, b;
+
+	while (s[a])
+	{
+		s[
