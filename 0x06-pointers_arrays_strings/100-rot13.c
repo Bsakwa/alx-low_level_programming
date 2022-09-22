edd2eb363dcbc7ac6c40f4fc6551491cd0ba73b2
Bsakwa <sakwabrian23@gmail.com>
2022-09-22 18:10:48 +0000

    Rot13 Trial

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,10 +5,23 @@
  *
  * return: The modified string
  */
-char *rot13(char *p)
+char *rot13(char *s)
 {
-	int a, b;
+	int a = 0;
 
 	while (s[a])
 	{
-		s[
+		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] = 'A' && s[a] <= 'Z'))
+		{
+			if ((s[a] > 'm' && s[a] <= 'z') || (s[a] = 'M' && s[a] <= 'Z'))
+			{
+				s[a] -= 13;
+				break;
+			}
+			s[a] += 13;
+			break;
+		}
+		a++;
+	}
+	return (s);
+}
