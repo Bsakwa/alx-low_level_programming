be728a4b4c19bb26ec0bd7e15deeac7866402166
Bsakwa <sakwabrian23@gmail.com>
2022-09-26 10:17:58 +0000

    Locate

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strchr - Locates a character in a string
+ *@s: string parameter
+ *@c: The character to be searched
+ *
+ * Return: Pointer to dest
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+	}
+	if (*(s + i) != c)
+	{
+		return (s + i);
+	}
+
+	return (0);
+}
