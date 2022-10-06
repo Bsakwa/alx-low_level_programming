762e0dba42f344a180a049f5b5c43d31cbb3c1b8
Bsakwa <sakwabrian23@gmail.com>
2022-10-06 18:54:56 +0000

    Last commit

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - multiplies numbers
+ *
+ * Return: The multiples of numbers
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	int mul;
+
+	for (i = 0; i < argc; i++)
+	{
+		mul *=atoi(argv[i]);
+	}
+	return (mul);
+}
