34d05305320b7daaaa6087ff36800470e1943e7b
Bsakwa <sakwabrian23@gmail.com>
2022-10-06 17:26:24 +0000

    Third Commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * calloc - allocates memory for an array
+ * @nmemb
+ * @size
+ *
+ * Return:NULL if nmemb or size is zero. NULL if malloc fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *b;
+	char *c;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	b = malloc(size * nmemb);
+	
+	if (b == NULL)
+	{
+		return (NULL);
+	}
+	c = b;
+	{
+		for (i = 0; i < (size *nmemb); i++)
+		{
+			c[i] = '\0';
+		}
+	}
+	return (b);
+}
