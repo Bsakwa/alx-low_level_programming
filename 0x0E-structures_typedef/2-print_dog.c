2296d3449c0fe0ea3f3a24a6f68a6aa59c4e256d
Bsakwa <sakwabrian23@gmail.com>
2022-10-11 15:03:14 +0000

    Free

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,6 +9,7 @@
  */
 void print_dog(struct dog *d)
 {
+
 	if (d == NULL)
 	{
 		return;
@@ -30,5 +31,7 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner);
+		d = malloc(sizeof(struct dog));
+		free(d);
 	}
 }
