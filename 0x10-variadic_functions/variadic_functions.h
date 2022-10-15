dc9a0d05e129a76969a08f2ddb182cf30dbd87e5
Bsakwa <sakwabrian23@gmail.com>
2022-10-15 09:44:12 +0000

    Header Update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,13 +2,17 @@
 #define VARIADIC_H
 
 #include <stdarg.h>
-
+/**
+ * struct print- prints the appropriate character or string
+ *@print: pointer to the print function
+ *@symbol:The symbol passed to be printed
+ */
 typedef struct print
 {
 	char *symbol;
-	void(*print)(va_list args);
+	void (*print)(va_list args);
 
-}printer_a;
+} printer_a;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
