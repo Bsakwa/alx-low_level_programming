aa9fd949f1f8d3a60f83e4bbfe09141544bde88e
Bsakwa <sakwabrian23@gmail.com>
2022-10-27 13:36:46 +0000

    Find Node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,13 +13,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	unsigned int count = 0;
 
-	if (head == NULL)
+	for (count = 0; count < index; count++)
 	{
-		return (NULL);
-	}
-	while (count < index)
-	{
-		count++;
+		if (head == NULL)
+		{
+			return (NULL);
+		}
 		head = head->next;
 	}
 	return (head);
