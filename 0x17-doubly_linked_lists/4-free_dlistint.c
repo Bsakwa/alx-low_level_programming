9d992a22def344fff95609104198161473650559
Bsakwa <sakwabrian23@gmail.com>
2022-12-08 11:13:01 +0000

    Frees a list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a linked list
+ * @head: The head of the linked list
+ *
+ * Return: Nothing
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
